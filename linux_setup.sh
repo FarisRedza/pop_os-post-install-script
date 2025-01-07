fddc563a9d21d5081e81de4b3191ffabf18de123
Faris Redza <faris.redza1999@gmail.com>
2025-01-07 23:31:04 +0000

    bug fixes and cleanup

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 USER=$(whoami)
-SCRIPT="Linux_Setup"
+SCRIPT="linux_setup"
 
 function select_distro {
 	PS3='Select distribution: '
@@ -17,7 +17,7 @@ function select_distro {
 		    ;;
 		"Ubuntu")
 		    echo "Selecting Ubuntu"
-			DISTRO="Ubuntu"
+			DISTRO="UBUNTU"
 			touch ~/ubuntu
 		    break
 		    ;;
@@ -88,13 +88,16 @@ function github_latest_release_deb() {
 	cd -
 }
 
-function enable_timeshift {
+function setup_timeshift {
+	echo "Setting up timeshift"
 	sudo apt-get install -y timeshift 
 	gnome-disks
 	sudo timeshift-gtk
+	echo "Timeshift setup complete"
 }
 
 function remove_packages {
+	echo "Removing packages"
 	local debian="gnome-games libreoffice-common evolution-common shotwell-common transmission-common zutty mlterm-common xiterm+thai"
 
 	local pop="libreoffice-common"
@@ -113,12 +116,14 @@ function remove_packages {
 	fi
 
 	sudo apt-get autopurge -y $distro
+	echo "Package removal complete"
 }
 
 function install_packages {
+	echo "Installing packages"
 	install_via
 	setup_nix
-    setup_docker
+	setup_docker
 
 	local system_apps="gparted virt-manager"
 
@@ -132,11 +137,11 @@ function install_packages {
 
 	local extras="fonts-ibm-plex"
 
-	local pop="synaptic ubuntu-restricted-extras webp-pixbuf-loader playerctl gnome-user-share gnome-sushi code tldr"
+	local pop="synaptic gnome-{sushi,user-share} webp-pixbuf-loader playerctl code tldr ubuntu-restricted-extras"
 
-	local ubuntu="synaptic gnome-sushi gnome-user-share nautilus-hide fastfetch distrobox ibus-typing-booster ubuntu-restricted-extras network-manger-*-gnome network-manager-strongswan libfuse2t64 gnome-epub-thumbnailer icoextract-thumbnailer"
+	local ubuntu="synaptic gnome-{sushi,user-share} libfuse2t64 curl nautilus-hide fastfetch distrobox ibus-typing-booster network-manager-{strongswan,{fortisslvpn,iodine,l2tp,openconnect,ssh,sstp,vpnc}-gnome} {gnome-epub,icoextract}-thumbnailer ubuntu-restricted-extras"
 
-	local debian="bash-completion command-not-found thermald distrobox fastfetch network-manger-*-gnome network-manager-strongswan ibus-typing-booster libavcodec-extra ttf-mscorefonts-installer unrar gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi libfuse2 tldr"
+	local debian="bash-completion command-not-found thermald libfuse2 tldr nautilus-hide fastfetch distrobox ibus-typing-booster network-manager-{strongswan,{fortisslvpn,iodine,l2tp,openconnect,ssh,sstp,vpnc}-gnome} {gnome-epub,icoextract}-thumbnailer libavcodec-extra ttf-mscorefonts-installer unrar gstreamer1.0-libav gstreamer1.0-plugins-ugly gstreamer1.0-vaapi"
 
 	if [ $DISTRO = "POP" ]
 	then
@@ -145,35 +150,38 @@ function install_packages {
 		local distro=$pop
 	elif [ $DISTRO = "UBUNTU" ]
 	then
-        setup_extrepo
+		setup_extrepo
 		setup_flatpak
-		install_snaps
 		local distro=$ubuntu
 	elif [ $DISTRO = "DEBIAN" ]
 	then
 		sudo apt-get install -y --no-install-recommends steam-devices
-        setup_extrepo
-        install_firefox
-        install_vscode
+		setup_extrepo
+		install_firefox
+		install_vscode
 		install_p3xonenote
 		setup_flatpak
 		local distro=$debian
 	fi
 
-	sudo apt-get install -y $system_apps $system_utilities $hardware_utilities $media_utilities $development $extras $distro
+	eval "sudo apt-get install -y $system_apps $system_utilities $hardware_utilities $media_utilities $development $extras $distro"
 
 	# Enable DVD playback
 	sudo apt-get -y install libdvd-pkg
 	sudo dpkg-reconfigure libdvd-pkg
+	echo "Package installation complete"
 }
 
 function setup_extrepo {
-    sudo apt-get install -y extrepo
+	echo "Setting up extrepo"
+	sudo apt-get install -y extrepo
 	sudo sed -i 's/# - contrib/- contrib/g' /etc/extrepo/config.yaml
 	sudo sed -i 's/# - non-free/- non-free/g' /etc/extrepo/config.yaml
+	echo "Extrepo setup complete"
 }
 
 function setup_flatpak {
+	echo "Setting up flatpak"
 	sudo apt-get install -y --no-install-recommends gnome-software-plugin-flatpak
 	flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 	if [ $DISTRO = "UBUNTU" ]
@@ -191,24 +199,31 @@ profile bwrap /usr/bin/bwrap flags=(unconfined) {
 EOF
 		sudo systemctl reload apparmor
 	fi
+	echo "Flatpak setup complete"
 }
 
 function install_p3xonenote {
+	echo "Installing p3x-onenote"
 	github_latest_release_deb patrikx3 onenote
  	sudo apt-get install -y ./p3x-onenote*.deb
   	rm -rfv ./p3x-onenote*.deb
+	echo "p3x-onenote installed"
 }
 
 function install_via {
+	echo "Installing via"
 	github_latest_release_deb the-via releases
  	sudo apt-get install -y ./via*.deb
   	rm -rfv ./via*.deb
+	echo "via installed"
 }
 
 function setup_nix {
+	echo "Setting up nix"
 	sudo apt-get install -y nix-setup-systemd
+	
+	echo "Adding user to nix-users"
 	sudo usermod -aG nix-users $USER
-	newgrp nix-users
 
 	# Nix integrations
 	mkdir ~/.config/nixpkgs
@@ -216,18 +231,18 @@ function setup_nix {
 
 	# Nix desktop icons
 	printf '# Nix package desktop icons\nXDG_DATA_DIRS="/home/faris/.nix-profile/share:$XDG_DATA_DIRS"' > ~/.config/environment.d/nixIcons.conf
+
+	echo "nix setup complete"
 }
 
 function setup_docker {
     sudo apt-get install -y docker.io
     sudo usermod -aG docker $USER
-    newgrp docker
 }
 
 function setup_nautilus_share {
 	sudo apt-get install -y nautilus-share
 	sudo usermod -aG sambashare $USER
-	newgrp sambashare
 }
 
 function install_firefox {
@@ -238,8 +253,8 @@ function install_firefox {
 }
 
 function install_nix_packages {
-	# Add unstable channel
-	nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
+	echo "Adding unstable channel"
+	nix-channel --add https://nixos.org/channels/nixpkgs-unstable nixpkgs
 	nix-channel --update
 
 	local android="nixpkgs.android-tools"
@@ -279,14 +294,17 @@ function install_nix_packages {
 }
 
 function install_snaps {
-	local utilities="tldr"
+	sudo snap refresh
 
-	local development="code rustup"
+	local utilities="tldr"
 
 	local office="thunderbird"
 
 	local game_launchers="steam"
+
 	sudo snap install $utilities $development $office $game_launchers
+	sudo snap install --classic code
+	sudo snap install --classic rustup
 }
 
 function install_flatpaks {
@@ -306,15 +324,15 @@ function install_flatpaks {
 
 	local games="io.mrarm.mcpelauncher com.github.k4zmu2a.spacecadetpinball org.gnome.Aisleriot org.gnome.Chess org.gnome.Mines org.gnome.Mahjongg org.gnome.Quadrapassel org.gnome.Sudoku org.gnome.TwentyFortyEight"
 
-    local game_launchers="com.valvesoftware.Steam com.heroicgameslauncher.hgl org.prismlauncher.PrismLauncher net.pcsx2.PCSX2 org.DolphinEmu.dolphin-emu info.cemu.Cemu com.dosbox_x.DOSBox-X"
+    local game_launchers="com.heroicgameslauncher.hgl org.prismlauncher.PrismLauncher net.pcsx2.PCSX2 org.DolphinEmu.dolphin-emu info.cemu.Cemu com.dosbox_x.DOSBox-X"
 
 	local game_utilities="com.github.Matoking.protontricks net.davidotek.pupgui2 org.freedesktop.Platform.VulkanLayer.MangoHud"
 
-	local pop="org.gtk.Gtk3theme.Pop org.gtk.Gtk3theme.Pop-dark org.goldendict.GoldenDict org.gnome.Maps org.gnome.clocks io.github.flattool.Warehouse "
+	local pop="org.gtk.Gtk3theme.Pop org.gtk.Gtk3theme.Pop-dark com.valvesoftware.Steam org.goldendict.GoldenDict org.gnome.Maps org.gnome.clocks io.github.flattool.Warehouse"
 
-	local ubuntu=""
+	local ubuntu="com.github.hugolabe.Wike org.gnome.Maps org.gnome.Calendar"
 
-	local debian="org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark com.github.hugolabe.Wike org.gnome.PowerStats"
+	local debian="org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark com.valvesoftware.Steam com.github.hugolabe.Wike org.gnome.PowerStats"
 
 	if [ $DISTRO = "POP" ]
 	then
@@ -356,21 +374,55 @@ function gaming_setup {
 	flatpak override --user --system-talk-name=org.freedesktop.NetworkManager com.valvesoftware.Steam
 }
 
+function autostart_script {
+	mkdir ~/.config/autostart
+	printf "[Desktop Entry]
+Type=Application
+Exec=/home/$USER/$SCRIPT.sh
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name=$SCRIPT
+Terminal=true" >> ~/.config/autostart/$SCRIPT.desktop
+}
+
 if [ ! -f ~/system_install ]
 then
-    sudo apt-get update
-    sudo apt-get full-upgrade -y
+	select_distro
+	sudo apt-get update
+	sudo apt-get full-upgrade -y
 
-    enable_timeshift
+	setup_timeshift
 
-    remove_packages
-    install_packages
-    install_nix_packages
+	remove_packages
+	install_packages
 
-    touch ~/system_install
+	if [ $DISTRO = "UBUNTU" ]
+	then
+		install_snaps
+	fi
+
+	touch ~/system_install
+	autostart_script
+	systemctl reboot
 fi
-if [ -f ~/system_install]
+if [ -f ~/system_install ]
 then
-    install_flatpaks
+	check_distro
+	install_flatpaks
+	install_nix_packages
 
+	rm -rfv ~/.config/autostart/$SCRIPT.desktop
+	rm -rfv ~/system_install
+	if [ $DISTRO = "POP" ]
+	then
+		rm -rfv ~/pop
+	elif [ $DISTRO = "UBUNTU" ]
+	then
+		rm -rfv ~/ubuntu
+	elif [ $DISTRO = "DEBIAN" ]
+	then
+		rm -rfv ~/debian
+	fi
+	systemctl reboot
 fi
